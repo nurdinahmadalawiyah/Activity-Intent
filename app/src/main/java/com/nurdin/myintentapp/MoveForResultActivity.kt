2f649f43888220a131bc629aff9ed4acbdfc28e2
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-01-30 18:31:58 +0700

    Implement Result Activity

---
+++ app/src/main/java/com/nurdin/myintentapp/MoveForResultActivity.kt
@@ -0,0 +1,11 @@
+package com.nurdin.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveForResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_for_result)
+    }
+}
