391505eb58f1f9148f915263873b0c058ecd7442
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-01-30 14:22:47 +0700

    first commit

---
+++ app/src/main/java/com/nurdin/myintentapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.nurdin.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveDataActivity.setOnClickListener(this)
+
+        val btnMoveDataObject:Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveDataObject.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Nurdin A. Alawiyah")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 23)
+                startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "Nurdin A. Alawiyah",
+                    23,
+                    "nurdinahmda@gmail.com",
+                    "Bandung"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+        }
+    }
+}
